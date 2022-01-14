2072048942669fff653e5e873444ed0860231a8b
tmxkqotnl <tmxkqotnl@gmail.com>
2022-01-14 12:40:55 +0900

    feat, select 함수 2개

--- controller/db_controller.py
+++ controller/db_controller.py
@@ -1,7 +1,6 @@
 from db.db import DB
 from lib.lib import error_logger, spread_dict
 from psycopg2 import sql
-import db.query as q
 from uuid import uuid4
 
 # @error_logger
@@ -15,18 +14,32 @@ from uuid import uuid4
 #     }
 #     data_str = spread_dict(data,',')
 #     q = "insert into rank values({})".format(data_str)
-    
-#     return db.execute_query_has_return(q) 
 
-    
+#     return db.execute_query_has_return(q)
+
+
 @error_logger
 def drop_database(db: DB, db_name: str) -> None:
     db.execute_query_no_return(
         sql.SQL("drop database if exists {};").format(sql.Identifier(db_name))
     )
 
+
 @error_logger
 def create_database(db: DB, db_name: str) -> None:
     db.execute_query_no_return(
         sql.SQL("create database {};").format(sql.Identifier(db_name))
     )
+
+
+@error_logger
+def select_top10_minimum_attemps(db: DB) -> list[list[tuple], list[str]]:
+    query = (
+        """select name, attemps from rank order by attemps desc limit 10;"""
+    )
+    return db.execute_query_has_return(query)
+
+@error_logger   
+def select_top10_minimum_time(db:DB) -> list[list[tuple], list[str]]:
+    query = """select name, min(end_dt - start_dt) as takes_time from rank group by name order by takes_time limit 5;"""
+    return db.execute_query_has_return(query)
