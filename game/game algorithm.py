41dfbf090f78e82d7c7fd4449d76478794478682
unknown <juhyunwoo23@naver.com>
2022-01-14 12:58:39 +0900

    game algorithm

--- game/game algorithm.py
+++ game/game algorithm.py
@@ -17,13 +17,20 @@ def Create_numbers() :
 
 #입력값 체크 #######
 def Check_input(num) :
-    num = []
+    num.split()
+
 
 #정답 체크
 def Check_number(num, number) :
     Strike = 0
     Ball = 0
-    
+    for i in range(0,4):
+        if num[i] in number :
+            if num[i] == number[i] :
+                Strike = Strike + 1
+            else :
+                Ball = Ball + 1
+    return Strike , Ball
 
 #게임 진행
 Count = 0  #시도 횟수
@@ -32,14 +39,13 @@ Ball = 0  #볼
 
 while Strike != 4 :
     num = input("0~9 사이의 중복되지 않는 숫자 4개를 입력하세요. ")
-    num.split()
-    if not Check_number( num ) :
+    if not Check_number(num) :
         print("0~9 사이의 중복되지 않는 숫자 4개를 입력하세요. ")
         continue
     count = count + 1
-    Strike, Ball = Check_number( num )
+    Strike, Ball = Check_number(num)
     print(" %d번째 시도 => %d Strike, %d Ball" %Count, Strike, Ball)
 
 print(" 4개의 숫자를 모두 맞추셨습니다. ")
 
-print(num.split())
+print(arr)
